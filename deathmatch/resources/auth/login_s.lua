c871841674c5d01d4bb7eca035a2d7a39e22f3cb
Neproify <yamasaki3214@gmail.com>
2016-05-31 16:05:24 +0200

    Use Flarum accounts, bcrypt required

--- deathmatch/resources/auth/login_s.lua
+++ deathmatch/resources/auth/login_s.lua
@@ -15,19 +15,18 @@ addEventHandler("onLoginRequest", root, function(login, password)
 		triggerClientEvent(client, "onLoginResult", root, result)
 		return
 	end
-
-	local salt = db:fetch("SELECT `salt` FROM `mybb_users` WHERE `username`=? LIMIT 1;", login)
-	salt = salt[1]["salt"]
-	if not salt then
+	
+	local globalInfoTemp = db:fetch("SELECT * FROM `forum_users` WHERE `username`=? LIMIT 1;", login)
+	globalInfoTemp = globalInfoTemp[1]
+	
+	if not globalInfoTemp then
 		result.success = false
 		result.message = "Podane konto nie istnieje w bazie danych. Sprawdź czy wpisany login jest poprawny."
 		triggerClientEvent(client, "onLoginResult", root, result)
 		return
 	end
-
-	local globalInfoTemp = db:fetch("SELECT `uid`, `username`, `score`, `admin`, `permissions` FROM `mybb_users` WHERE `username`=? AND `password`=md5(CONCAT(md5(?),md5(?))) LIMIT 1;", login, salt, password)
-	globalInfoTemp = globalInfoTemp[1]
-	if not globalInfoTemp then
+	
+	if not bcrypt_verify(password, globalInfoTemp["password"]) then
 		result.success = false
 		result.message = "Podałeś nieprawidłowy login i/lub hasło."
 		triggerClientEvent(client, "onLoginResult", root, result)
@@ -35,7 +34,7 @@ addEventHandler("onLoginRequest", root, function(login, password)
 	end
 
 	local globalInfo = {}
-	globalInfo["UID"] = globalInfoTemp["uid"]
+	globalInfo["UID"] = globalInfoTemp["id"]
 	globalInfo["name"] = globalInfoTemp["username"]
 	globalInfo["score"] = globalInfoTemp["score"]
 	globalInfo["admin"] = globalInfoTemp["admin"]
