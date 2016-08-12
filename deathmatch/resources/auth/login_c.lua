7073e6cc8e2ee25bd73df6e34f10f5787cdbac75
Neproify <yamasaki3214@gmail.com>
2016-08-12 14:05:19 +0200

    Fixed login input issues

--- deathmatch/resources/auth/login_c.lua
+++ deathmatch/resources/auth/login_c.lua
@@ -1,6 +1,8 @@
 local loginWindow = nil
 local screenWidth, screenHeight = guiGetScreenSize()
 
+local processingLogin = false
+
 addEventHandler("onClientResourceStart", resourceRoot, function()
 	if localPlayer:getData("globalInfo") and localPlayer:getData("charInfo") then
 		return
@@ -13,29 +15,32 @@ addEventHandler("onClientResourceStart", resourceRoot, function()
 		return
 	end
 	
-	-- Timer, bugował się input, tymczasowe?
-	Timer(function()
-		loginWindow = GuiBrowser(screenWidth / 2 - 150, screenHeight / 2 - 100, 300, 200, true, true, false)
-		addEventHandler("onClientBrowserCreated", loginWindow, function()
-			addEventHandler("onClientBrowserDocumentReady", root, function(url)
-				if url ~= "http://mta/local/login.html" then return end
-				Camera.fade(true)
-				Camera.setMatrix(1489.8495, -1690.1045, 14.5469, 1484.8495, -1694.1045, 14.5469)
-				showChat(false)
-				showCursor(true)
-				guiSetInputEnabled(true)
-				addEvent("onLoginForm", true)
-				addEventHandler("onLoginForm", loginWindow:getBrowser(), function(login, password)
-					triggerServerEvent("onLoginRequest", root, login, password)
-				end)
+	loginWindow = GuiBrowser(screenWidth / 2 - 150, screenHeight / 2 - 100, 300, 200, true, true, false)
+	addEventHandler("onClientBrowserCreated", loginWindow, function()
+		addEventHandler("onClientBrowserDocumentReady", root, function(url)
+			if url ~= "http://mta/local/login.html" then return end
+			Camera.fade(true)
+			Camera.setMatrix(1489.8495, -1690.1045, 14.5469, 1484.8495, -1694.1045, 14.5469)
+			showChat(false)
+			showCursor(true)
+			guiSetInputEnabled(true)
+			loginWindow:bringToFront(true)
+			addEvent("onLoginForm", true)
+			addEventHandler("onLoginForm", loginWindow:getBrowser(), function(login, password)
+				if processingLogin == true then
+					return
+				end
+				processingLogin = true
+				triggerServerEvent("onLoginRequest", root, login, password)
 			end)
-			loginWindow:getBrowser():loadURL("http://mta/local/login.html")
 		end)
-	end, 5000, 1)
+		loginWindow:getBrowser():loadURL("http://mta/local/login.html")
+	end)
 end)
 
 addEvent("onLoginResult", true)
 addEventHandler("onLoginResult", root, function(result)
+	processingLogin = false
 	if not result.success then
 		exports.notifications:add(result.message, "danger", 5000)
 		return
