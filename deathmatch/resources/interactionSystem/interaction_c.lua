4ba23c8aee9da29fc6a072fee0b2471ecbf8f49e
Neproify <yamasaki3214@gmail.com>
2016-08-21 14:33:42 +0200

    Powrót do starego systemu interakcji

--- deathmatch/resources/interactionSystem/interaction_c.lua
+++ deathmatch/resources/interactionSystem/interaction_c.lua
@@ -1,74 +1,56 @@
 local screenWidth, screenHeight = guiGetScreenSize()
 
-local selectedElement = nil
-local interactionActive = false
-local interactionWindow = nil
-
-addEventHandler("onClientResourceStart", resourceRoot, function()
-	interactionWindow = GuiBrowser(0, 0, screenWidth, screenHeight, true, true, false)
-	addEventHandler("onClientBrowserCreated", interactionWindow, function()
-		interactionWindow:getBrowser():loadURL("http://mta/local/interaction.html")
-		guiSetVisible(interactionWindow, interactionActive)
-		bindKeysForPointer()
-	end)
-end)
-
-function drawPointer()
-	dxDrawCircle(screenWidth / 2, screenHeight / 2, 1, 3, 1, 0, 360, tocolor(255, 0, 0))
+function showInteractionCircle()
+	dxDrawCircle(screenWidth / 2, screenHeight / 2 - 150, 1, 3, 1, 0, 360, tocolor(255, 0, 0))
 end
 
-function interactionShowPointer()
-	selectedElement = nil
-	addEventHandler("onClientRender", root, drawPointer)
+function showInteraction()
+	addEventHandler("onClientRender", root, showInteractionCircle)
 end
 
-function interactionGetElement()
-	removeEventHandler("onClientRender", root, drawPointer)
-	getNearbyElementForInteraction()
-	if not selectedElement then
-		return
-	end
-	interactionActive = true
-	guiSetVisible(interactionWindow, interactionActive)
-	showCursor(true)
-	bindKeysForMenu()
-end
-
-function cancelInteraction()
-	interactionActive = false
-	guiSetVisible(interactionWindow, interactionActive)
-	bindKeysForPointer()
-	showCursor(false)
-end
-
-function bindKeysForPointer()
-	unbindKey("mouse2", "down", cancelInteraction)
-
-	bindKey("1", "down", interactionShowPointer)
-	bindKey("1", "up", interactionGetElement)
-end
-
-function bindKeysForMenu()
-	unbindKey("1", "down", interactionShowPointer)
-	unbindKey("1", "up", interactionGetElement)
-	
-	bindKey("mouse2", "down", cancelInteraction)
-end
-
-function getNearbyElementForInteraction()
-	local px, py, pz = getCameraMatrix()
-	local tx, ty, tz = getWorldFromScreenPosition(screenWidth / 2, screenHeight / 2, 10)
-	hit, x, y, z, elementHit = processLineOfSight(px, py, pz, tx, ty, tz, true, true, true, true, true, false, false, false, localPlayer)
+function endInteraction()
+	removeEventHandler("onClientRender", root, showInteractionCircle)
+	x, y, z = getCameraMatrix()
+	x2, y2, z2 = getWorldFromScreenPosition(screenWidth / 2, screenHeight / 2 - 150, 10)
+	hit, hitX, hitY, hitZ, hitElement, normalX, normalY, normalZ, material, lighting, piece = processLineOfSight(x, y, z, x2, y2, z2 - 1.5, true, true, true, true, true, true, false, true, localPlayer, false, true)
 	if hit then
-		if elementHit then
-			selectedElement = elementHit
-		else
-			return false
+		if hitElement then
+			if hitElement.type == "vehicle" then
+				if piece == 2 then -- bagażnik
+					if hitElement:getDoorOpenRatio(1) > 0 then
+						if not hitElement.locked then
+							hitElement:setDoorOpenRatio(1, 0, 1000)
+						end
+					else
+						if not hitElement.locked then
+							hitElement:setDoorOpenRatio(1, 1, 1000)
+						end
+					end
+				elseif piece == 3 then -- maska
+					if hitElement:getDoorOpenRatio(0) > 0 then
+						if not hitElement.locked then
+							hitElement:setDoorOpenRatio(0, 0, 1000)
+						end
+					else
+						if not hitElement.locked then
+							hitElement:setDoorOpenRatio(0, 1, 1000)
+						end
+					end
+				end
+			elseif hitElement.type == "object" then
+				if hitElement:getData("itemInfo") then -- przedmiot
+					local itemInfo = hitElement:getData("itemInfo")
+					triggerServerEvent("pickItemByPlayer", localPlayer, itemInfo.UID)
+				end
+			end
 		end
-	else
-		return false
 	end
 end
+
+bindKey("1", "down", showInteraction)
+bindKey("1", "up", endInteraction)
+
+--funkcja do rysowania "kółek"
 
 function dxDrawCircle( posX, posY, radius, width, angleAmount, startAngle, stopAngle, color, postGUI )
 	if ( type( posX ) ~= "number" ) or ( type( posY ) ~= "number" ) then
