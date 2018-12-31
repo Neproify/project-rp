41a4c81c75f15f896433cf82668926d6b7375c66
Neproify <yamasaki3214@gmail.com>
2018-12-31 02:23:07 +0100

    Semantic UI. Items rebuilt. Looks better. :)

--- deathmatch/resources/items/playerItems.js
+++ deathmatch/resources/items/playerItems.js
@@ -5,27 +5,29 @@ function updateItems()
 	$('#items tbody').html('');
 	for(var i = 0; i < items.length; i++)
 	{
-		var output = '<tr id="'+items[i]['UID']+'">';
+		var output = '<tr>';
 		output += '<td>'+items[i]['UID']+'</td>';
-		output += '<td id="useItem">'+items[i]['name']+'</td>';
-		output += '<td id="dropItem">Wyrzuć</td>';
+		output += '<td id="useItem-'+items[i]['UID']+'">'+items[i]['name']+'</td>';
+		output += '<td><div class="ui dropdown"><div class="default text">Opcje</div>';
+		output += '<div class="menu"><div class="item" id="dropItem-'+items[i]['UID']+'">Wyrzuć</div></div>';
+		output += '</div></td>';
 		output += '</tr>';
 		var item = $(output);
 		item.appendTo('#items tbody');
+		$('.ui.dropdown').dropdown();
 		if(items[i]['used'] == true)
 		{
 			item.css('font-weight', '900');
 		}
 		item.on('click', function(event)
 		{
-			var parent = $(event.target).parent();
-			if(event.target.id == "useItem")
+			if(event.target.id.includes('useItem'))
 			{
-				useItem(parent.attr('id'));
+				useItem(event.target.id.substring(8));
 			}
-			if(event.target.id == "dropItem")
+			if(event.target.id.includes('dropItem'))
 			{
-				dropItem(parent.attr('id'));
+				dropItem(event.target.id.substring(9));
 			}
 		});
 	}
